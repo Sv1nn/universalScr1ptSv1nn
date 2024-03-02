b487e3996e3cf5770fcb53e3af3d0282dafeae33
Sv1nn <159580566+Sv1nn@users.noreply.github.com>
2024-03-02 22:52:40 +0300

    Update Un1vers.lua

--- Un1vers.lua
+++ Un1vers.lua
@@ -603,6 +603,28 @@ end)
     
     })
 
+    Tab:AddButton({
+        Name = "bTools",
+        Callback = function()
+            backpack = game:GetService("Players").LocalPlayer.Backpack
+
+            hammer = Instance.new("HopperBin")
+            hammer.Name = "Hammer"
+            hammer.BinType = 4
+            hammer.Parent = backpack
+            
+            cloneTool = Instance.new("HopperBin")
+            cloneTool.Name = "Clone"
+            cloneTool.BinType = 3
+            cloneTool.Parent = backpack
+            
+            grabTool = Instance.new("HopperBin")
+            grabTool.Name = "Grab"
+            grabTool.BinType = 2
+            grabTool.Parent = backpack
+          end    
+    })
+
 -- visual tab
 
 local VisualTab = Window:MakeTab({
