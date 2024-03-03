2ad576cd9345c74939788a8f59e65be18e31476f
Sv1nn <159580566+Sv1nn@users.noreply.github.com>
2024-03-03 20:06:42 +0300

    Update Un1vers.lua

--- Un1vers.lua
+++ Un1vers.lua
@@ -89,6 +89,26 @@ PlayerTab:AddButton({
           end    
     })
 
+    Tab:AddButton({
+        Name = "invisible",
+        Callback = function()
+            local player = game.Players.LocalPlayer
+            local character = player.Character or player.CharacterAdded:Wait()
+            local humanoid = character:WaitForChild("Humanoid")
+            
+            local function PTransparency(transparency)
+                for _, part in ipairs(character:GetDescendants()) do
+                    if part:IsA("BasePart") and part.Name ~= "HumanoidRootPart" then
+                        part.Transparency = transparency
+                    end
+                end
+            end
+            
+            
+            PTransparency(1)            
+          end    
+    })
+
     PlayerTab:AddButton({
         Name = "FlyV1",
         Callback = function()
