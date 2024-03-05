2c0d06c812caf8500d30909744e4056b4b2d53b6
Sv1nn <159580566+Sv1nn@users.noreply.github.com>
2024-03-05 22:32:04 +0300

    Update Un1vers.lua

--- Un1vers.lua
+++ Un1vers.lua
@@ -890,21 +890,28 @@ local Section = BreakTab:AddSection({
 BreakTab:AddButton({
 	Name = "AntiDieV1",
 	Callback = function()
-        game.Players.LocalPlayer.CharacterAdded:Connect(function(character)
-            while true do
-                wait(1)
-                character.HumanoidRootPart.CFrame = CFrame.new(-12, 3, -242, -0.7, 2.5e-11, 0.5, 1.e-10, 1, 5.5e-11, -0.5, 1.e-10, -0)
-                
-                wait(1)
-                character.HumanoidRootPart.CFrame = CFrame.new(-48, 3, -177, 0.7, 2.5e-11, -0.5, -10.e-11, 1, -8.5e-11, 0.5, 1.e-10, 0)
-                
-                wait(1)
-                character.HumanoidRootPart.CFrame = CFrame.new(-12, 3, -177, 0.7, 2.5e-11, 0.5, 7.e-11, 1, -1.e-10, -0.5, 1.e-10, 0)
-                
-                wait(1)
-                character.HumanoidRootPart.CFrame = CFrame.new(-48, 3, -242, -0.7, 2.5e-11, -0.5, -4.5e-11, 1, 1.e-10, 0.5, 1.e-10, -0)
-            end
-        end)
-               
+        local RunService = game:GetService("RunService")
+
+local function moveCharacter()
+    local char = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:wait()
+    
+    char.HumanoidRootPart.CFrame = CFrame.new(-12, 3, -242, -0.7, 2.5e-11, 0.5, 1.e-10, 1, 5.5e-11, -0.5, 1.e-10, -0)
+    
+    RunService.Heartbeat:wait()
+    
+    char.HumanoidRootPart.CFrame = CFrame.new(-48, 3, -177, 0.7, 2.5e-11, -0.5, -10.e-11, 1, -8.5e-11, 0.5, 1.e-10, 0)
+    
+    RunService.Heartbeat:wait()
+    
+    char.HumanoidRootPart.CFrame = CFrame.new(-12, 3, -177, 0.7, 2.5e-11, 0.5, 7.e-11, 1, -1.e-10, -0.5, 1.e-10, 0)
+    
+    RunService.Heartbeat:wait()
+    
+    char.HumanoidRootPart.CFrame = CFrame.new(-48, 3, -242, -0.7, 2.5e-11, -0.5, -4.5e-11, 1, 1.e-10, 0.5, 1.e-10, -0)
+end
+
+RunService.Heartbeat:Connect(function()
+    moveCharacter()
+end)
   	end    
 })
