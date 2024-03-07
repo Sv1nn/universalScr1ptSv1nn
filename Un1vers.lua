084aae04135904c390bd310bc8fc219328652251
Sv1nn <159580566+Sv1nn@users.noreply.github.com>
2024-03-07 20:30:39 +0300

    Update Un1vers.lua

--- Un1vers.lua
+++ Un1vers.lua
@@ -26,7 +26,7 @@ local Section = PlayerTab:AddSection({
 PlayerTab:AddButton({
     Name = "Speedhack",
     Callback = function()
-     while true do
+    while true do
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
     OrionLib:MakeNotification({
     Name = "SvinnHub",
