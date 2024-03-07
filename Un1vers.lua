d8f6eb81d588cde46ced54aff11869c4db125fbc
Sv1nn <159580566+Sv1nn@users.noreply.github.com>
2024-03-07 19:42:22 +0300

    Update Un1vers.lua

--- Un1vers.lua
+++ Un1vers.lua
@@ -904,10 +904,13 @@ local function moveCharacter()
     RunService.Heartbeat:wait()
     
     char.HumanoidRootPart.CFrame = CFrame.new(-10.6385431, -0.36107114, -176.790649, 0.776075244, 0.0452038422, 0.629018188, 3.38150286e-09, 0.997427762, -0.0716792792, -0.630640328, 0.0556285158, 0.774078965)
-    
     RunService.Heartbeat:wait()
     
     char.HumanoidRootPart.CFrame = CFrame.new(-10.6385431, -0.36107114, -176.790649, 0.776075244, 0.0452038422, 0.629018188, 3.38150286e-09, 0.997427762, -0.0716792792, -0.630640328, 0.0556285158, 0.774078965)
 end
-})
 
+RunService.Heartbeat:Connect(function()
+    moveCharacter()
+end)
+  	end    
+})
