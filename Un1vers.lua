d78edba503b155f894c2eb9437477e3855583883
Sv1nn <159580566+Sv1nn@users.noreply.github.com>
2024-03-14 21:40:10 +0300

    Update Un1vers.lua

--- Un1vers.lua
+++ Un1vers.lua
@@ -963,6 +963,7 @@ local Section = WestTab:AddSection({
   WestTab:AddButton({
 	Name = "AutofarmV1",
 	Callback = function()
+while true do
         local args = {
             [1] = "Safe",
             [2] = workspace:WaitForChild("Safe")
@@ -970,4 +971,5 @@ local Section = WestTab:AddSection({
         
         game:GetService("ReplicatedStorage"):WaitForChild("GeneralEvents"):WaitForChild("Rob"):FireServer(unpack(args))
   	end    
+end
 })
