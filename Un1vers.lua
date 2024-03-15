f62fb8399733235b1050c6a9222d3415cbe0fae8
Sv1nn <159580566+Sv1nn@users.noreply.github.com>
2024-03-15 18:13:58 +0300

    Update Un1vers.lua

--- Un1vers.lua
+++ Un1vers.lua
@@ -998,16 +998,7 @@ WestTab:AddButton({
             
             char.HumanoidRootPart.CFrame = CFrame.new(1627.92297, 128.849976, 1579.78149, -0.0333881564, -3.30991874e-08, -0.999442458, 2.13044915e-09, 1, -3.31888224e-08, 0.999442458, -3.23737481e-09, -0.0333881564)
             
-            RunService.Heartbeat:wait()
-            
-            char.HumanoidRootPart.CFrame = CFrame.new(1627.92297, 128.849976, 1579.78149, -0.0333881564, -3.30991874e-08, -0.999442458, 2.13044915e-09, 1, -3.31888224e-08, 0.999442458, -3.23737481e-09, -0.0333881564)
-            
-            RunService.Heartbeat:wait()
-            
-            char.HumanoidRootPart.CFrame = CFrame.new(1627.92297, 128.849976, 1579.78149, -0.0333881564, -3.30991874e-08, -0.999442458, 2.13044915e-09, 1, -3.31888224e-08, 0.999442458, -3.23737481e-09, -0.0333881564)
-            RunService.Heartbeat:wait()
-            
-            char.HumanoidRootPart.CFrame = CFrame.new(1627.92297, 128.849976, 1579.78149, -0.0333881564, -3.30991874e-08, -0.999442458, 2.13044915e-09, 1, -3.31888224e-08, 0.999442458, -3.23737481e-09, -0.0333881564)
+            RunService.Heartbeat:wait(5)
         end
         
         RunService.Heartbeat:Connect(function()
