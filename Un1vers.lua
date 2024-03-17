91ab7bcabdccdd04621742a4461eaa626e20c19e
Sv1nn <159580566+Sv1nn@users.noreply.github.com>
2024-03-17 20:45:45 +0300

    Update Un1vers.lua

--- Un1vers.lua
+++ Un1vers.lua
@@ -1,4 +1,3 @@
-
 -- Sv1nn Hub
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
@@ -604,151 +603,6 @@ end)
     
     })
 
-    PlayerTab:AddButton({
-        Name = "InVisible",
-        Callback = function()
-                  -- Ranxware | FE Invisibility Module | Version: Beta V1 --
-
-local InvisibilitySettings = {
-	TransparentClone = true, -- Makes the clone's body transparent
-	QuickMode = true, -- Forces module to be quicker
-	ChatNotifier = true, -- Makes notifications in chat (new chat version only)
-	Fly = false, -- Makes you fly when invisible
-	FlySpeed = 50, -- Optimizes fly speed
-	NoClip = false, -- Makes you noclip when invisible
-}
-
-
---[ PLEASE DO NOT TOUCH ANYTHING BELOW IF YOU DON'T KNOW WHAT YOU'RE DOING!! ]--
-
-local noclipCon = nil
-local flyCon = nil
-
-if game.TextChatService.ChatVersion == Enum.ChatVersion.LegacyChatService then
-	InvisibilitySettings.ChatNotifier = false
-end
-
-
-local lplr = game.Players.LocalPlayer
-local char = lplr.Character
-char.Archivable = true
-
-print("Starting cloning module...")
-if InvisibilitySettings.ChatNotifier then
-	game.TextChatService.ChatInputBarConfiguration.TargetTextChannel:DisplaySystemMessage("Invisibility | Starting cloning phase...")
-end
-
-workspace.CurrentCamera.CameraSubject = nil
-local oldChar = char or lplr.Character
-local oldCFrame = char:GetPivot()
-
-task.wait(InvisibilitySettings.QuickMode and .1 or 1.5)
-
--- old char protection --
-oldChar:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(1e5,1e6,1e5)
-
-task.wait(.1)
-
-oldChar:FindFirstChild("HumanoidRootPart").Anchored = true
-
-task.wait(.5)
-
-local clone = char:Clone()
-clone.Parent = workspace
-lplr.Character = clone
-workspace.CurrentCamera.CameraSubject = clone:FindFirstChildOfClass("Humanoid")
-clone:FindFirstChild("HumanoidRootPart").CFrame = oldCFrame
-clone:FindFirstChild("HumanoidRootPart").Anchored = false
-
-task.wait()
-
-if InvisibilitySettings.TransparentClone then
-	for _,i in next,clone:GetDescendants() do
-		if i:IsA("BasePart") and i.Name ~= "HumanoidRootPart" then
-			i.Transparency = 0.5
-		end
-	end
-end
-
-if clone:FindFirstChild("Animate") then
-	clone.Animate.Disabled = true
-	task.wait()
-	clone.Animate.Disabled = false
-end
-
-if InvisibilitySettings.Fly then
-	if InvisibilitySettings.ChatNotifier then
-		game.TextChatService.ChatInputBarConfiguration.TargetTextChannel:DisplaySystemMessage("Invisibility | Stored fly is enabled!")
-		game.TextChatService.ChatInputBarConfiguration.TargetTextChannel:DisplaySystemMessage("Invisibility | Use Space and LeftCtrl to go up and down...")
-	end
-	local vel = Instance.new("BodyVelocity",clone:FindFirstChild("HumanoidRootPart"))
-	vel.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
-	flyCon = game:GetService("RunService").RenderStepped:Connect(function()
-		vel.Velocity =  clone:FindFirstChild("Humanoid").MoveDirection * InvisibilitySettings.FlySpeed
-		if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.LeftControl) then
-			vel.Velocity = Vector3.new(vel.Velocity.X,-InvisibilitySettings.FlySpeed,vel.Velocity.Z)
-		elseif game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.Space) then
-			vel.Velocity = Vector3.new(vel.Velocity.X,InvisibilitySettings.FlySpeed,vel.Velocity.Z)
-		end
-	end)
-end
-
-if InvisibilitySettings.NoClip then
-	if InvisibilitySettings.ChatNotifier then
-		game.TextChatService.ChatInputBarConfiguration.TargetTextChannel:DisplaySystemMessage("Invisibility | Stored noclip is enabled!")
-	end
-	noclipCon = game:GetService("RunService").Stepped:Connect(function()
-		for _, child in next,clone:GetDescendants() do
-			if child:IsA("BasePart") and child.CanCollide == true then
-				child.CanCollide = false
-			end
-		end
-	end)
-end
-
-clone:FindFirstChildOfClass("Humanoid").DisplayName = clone.Name.." [INVISIBLE]"
-
-print("Cloning module success!")
-if InvisibilitySettings.ChatNotifier then
-	game.TextChatService.ChatInputBarConfiguration.TargetTextChannel:DisplaySystemMessage("Invisibility | You are now invisible!")
-	game.TextChatService.ChatInputBarConfiguration.TargetTextChannel:DisplaySystemMessage("Invisibility | Reset to return normal")
-end
-
-task.wait()
-
-local function revert()
-	task.wait()
-	if InvisibilitySettings.ChatNotifier then
-		game.TextChatService.ChatInputBarConfiguration.TargetTextChannel:DisplaySystemMessage("Invisibility | Reverting to normal character...")
-	end
-	local clonePos = clone:GetPivot()
-	task.wait(InvisibilitySettings.QuickMode and .2 or .5)
-	if flyCon then
-		flyCon:Disconnect()
-	end
-	if noclipCon then
-		noclipCon:Disconnect()
-	end
-	task.wait(.1)
-	clone:Destroy()
-	workspace.CurrentCamera.CameraSubject = oldChar:FindFirstChildOfClass("Humanoid")
-	task.wait()
-	oldChar:FindFirstChild("HumanoidRootPart").Anchored = false
-	lplr.Character = oldChar
-	lplr.Character:PivotTo(clonePos)
-	task.wait(1)
-	return
-end
-
-char = clone
-char:FindFirstChildOfClass("Humanoid").Died:Connect(function()
-	revert()
-end)
-
-
-          end    
-    })
-
 -- visual tab
 
 local VisualTab = Window:MakeTab({
@@ -1062,7 +916,7 @@ end)
 })
 
 BreakTab:AddButton({
-    Name = "AutoMurder",
+    Name = "Auto Murder",
     Callback = function()
     while true do
     local args = {
@@ -1082,7 +936,7 @@ end
 })
 
 BreakTab:AddButton({
-    Name = "DisiguinMe",
+    Name = "Disiguin me",
     Callback = function()
         local args = {
             [1] = 42,
@@ -1091,21 +945,6 @@ BreakTab:AddButton({
 
         game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
     end
-})
-
-BreakTab:AddButton({
-	Name = "autoOpenCase(leg. knife)",
-	Callback = function()
-        while true do
-            local args = {
-                [1] = 66,
-                [2] = "Knife Skins",
-                [3] = "Legendary Case"
-            }
-            
-            game:GetService("ReplicatedStorage"):WaitForChild("RemoteFunction"):InvokeServer(unpack(args))
-            end
-  	end    
 })
 
 
@@ -1131,7 +970,7 @@ local Section = WestTab:AddSection({
             }
             
             game:GetService("ReplicatedStorage"):WaitForChild("GeneralEvents"):WaitForChild("Rob"):FireServer(unpack(args))
-            wait(0.1)
+            wait(3)
         end
     end
 })
@@ -1144,7 +983,7 @@ WestTab:AddButton({
                 [1] = "Complete"
             }
             workspace:WaitForChild("Safe"):WaitForChild("OpenSafe"):FireServer(unpack(args))
-            wait(10)
+            wait(20)
         end
     end
 })
@@ -1155,11 +994,11 @@ WestTab:AddButton({
         local RunService = game:GetService("RunService")
 
         local function moveCharacter()
-            local char = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:wait( )
+            local char = game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:wait(5)
             
             char.HumanoidRootPart.CFrame = CFrame.new(1627.92297, 128.849976, 1579.78149, -0.0333881564, -3.30991874e-08, -0.999442458, 2.13044915e-09, 1, -3.31888224e-08, 0.999442458, -3.23737481e-09, -0.0333881564)
             
-            RunService.Heartbeat:wait()
+            RunService.Heartbeat:wait(5)
         end
         
         RunService.Heartbeat:Connect(function()
@@ -1179,23 +1018,7 @@ WestTab:AddButton({
             }
             
             game:GetService("ReplicatedStorage"):WaitForChild("GeneralEvents"):WaitForChild("Spawn"):FireServer(unpack(args))
-            wait(10)
-        end
-  	end    
-})
-
-WestTab:AddButton({
-	Name = "AspawnModTest",
-	Callback = function()
-        while true do
-            local args = {
-                [1] = "FortArthur",
-                [2] = true,
-                [3] = false
-            }
-            
-            game:GetService("ReplicatedStorage"):WaitForChild("GeneralEvents"):WaitForChild("Spawn"):FireServer(unpack(args))
-            wait(10)
+            wait(15)
         end
   	end    
 })
